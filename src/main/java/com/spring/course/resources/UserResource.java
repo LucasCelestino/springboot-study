52e594af6f57f5187cbef81743c8e8e15164c88f
Lucas Celestino <lucascap899@gmail.com>
2024-03-04 16:24:26 -0300

    User entity and resource

---
+++ src/main/java/com/spring/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.spring.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource
+{
+	@GetMapping
+	public ResponseEntity<User> findAll()
+	{
+		User u = new User(1L, "Lucas", "lucas@email.com", "3333-3333", "12345");
+		
+		return ResponseEntity.ok(u);
+	}
+}
