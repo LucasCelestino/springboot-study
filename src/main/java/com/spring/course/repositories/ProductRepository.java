3e628044d0c46494bb05dc7efb58091b4aa59371
Lucas Celestino <lucascap899@gmail.com>
2024-03-09 16:21:01 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/spring/course/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.spring.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {}
