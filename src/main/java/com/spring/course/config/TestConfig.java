2a3a8b1dcf15c226824c19851c8a57fd8f57c3b1
Lucas Celestino <lucascap899@gmail.com>
2024-03-09 17:43:45 -0300

    Payment, one-to-one association

--- src/main/java/com/spring/course/config/TestConfig.java
+++ src/main/java/com/spring/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.spring.course.entities.Category;
 import com.spring.course.entities.Order;
 import com.spring.course.entities.OrderItem;
+import com.spring.course.entities.Payment;
 import com.spring.course.entities.Product;
 import com.spring.course.entities.User;
 import com.spring.course.entities.enums.OrderStatus;
@@ -77,5 +78,11 @@ public class TestConfig implements CommandLineRunner
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment py1 = new Payment(null, Instant.parse("2019-07-21T04:42:10Z"), o2);
+		
+		o1.setPayment(py1);
+		
+		orderRepository.save(o1);
 	}
 }
